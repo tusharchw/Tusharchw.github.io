cd8df94ba1b78bc77d9e758c610245d3aee2d1fd
tusharchw <tusharchw@gmail.com>
2024-10-25 18:45:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function navigateTo(){
+    window.location.href="assets/project.htm";
+}
+
+function openGraphics(){
+    window.open("https://www.behance.net/tusharchw");
+}
+
+function instagramClick(){
+    window.open("https://www.instagram.com/tushar_chw/?hl=en");
+}
+
+function linkedinClick(){
+    window.open("https://www.linkedin.com/in/tushar-chawla-4358269a/");
+}
+
+function mediumClick(){
+    window.open("https://medium.com/@tusharchw");
+}
